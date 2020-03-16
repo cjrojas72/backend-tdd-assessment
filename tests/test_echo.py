6e27ed638e542a10faa04874992e97995ac0b274
Christian Rojas <cjrojas72@gmail.com>
2020-03-16 19:54:14 -0400

    save work

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -10,41 +10,98 @@ import subprocess
 
 class TestEcho(unittest.TestCase):
     def setUP(self):
-        pass
+        self.parser = echo.create_parser()
+    
+    # def tests_no_options(self):
+    #     #self.fail("Need to write test")
+    #     args = ["HelLo WoRld"]
+    #     ns = self.parser.parse_args(args)
+    #     self.assertFalse(ns.upper)
+    #     self.assertFalse(ns.lower)
+    #     self.assertFalse(ns.title)
+    #     actual = echo.main(args)
+    #     expected = args[0]
+    #     self.assertEqual(actual, expected)
+    
+    # def tests_all_options(self):
+    #     #self.fail("Need to write test")
+    #     args = ["-tul", "HelLo WoRld"]
+    #     ns = self.parser.parse_args(args)
+    #     self.assertTrue(ns.upper)
+    #     self.assertTrue(ns.lower)
+    #     self.assertTrue(ns.title)
+    #     actual = echo.main(args)
+    #     expected = "Hello World"
+    #     self.assertEqual(actual, expected)
 
     def tests_upper_short(self):
-        self.fail("Need to write test")
-    
+        #self.fail("Need to write test")
+        args = ["-u", "hello world"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+        actual = echo.main(args)
+        expected = "HELLO WORLD"
+        self.assertEqual(actual, expected)
+
     def tests_lower_short(self):
-        self.fail("Need to write test")
+        #self.fail("Need to write test")
+        args = ["-l", "hello world"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
+        actual = echo.main(args)
+        expected = "hello world"
+        self.assertEqual(actual, expected)
 
     def tests_title_short(self):
-        self.fail("Need to write test")
+        #self.fail("Need to write test")
+        args = ["-t", "HelLo WoRld"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.title)
+        actual = echo.main(args)
+        expected = "Hello World"
+        self.assertEqual(actual, expected)
 
     def tests_title_long(self):
-        self.fail("Need to write test")
+        #self.fail("Need to write test")
+        args = ["--title", "HelLo WoRld"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.title)
+        actual = echo.main(args)
+        expected = "Hello World"
+        self.assertEqual(actual, expected)
     
-    def tests_upper_longt(self):
-        self.fail("Need to write test")
+    def tests_upper_long(self):
+        #self.fail("Need to write test")
+        args = ["--upper", "hello world"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.upper)
+        actual = echo.main(args)
+        expected = "HELLO WORLD"
+        self.assertEqual(actual, expected)
     
     def tests_lower_long(self):
-        self.fail("Need to write test")
+        #self.fail("Need to write test")
+        args = ["--lower", "hello world"]
+        ns = self.parser.parse_args(args)
+        self.assertTrue(ns.lower)
+        actual = echo.main(args)
+        expected = "hello world"
+        self.assertEqual(actual, expected)
 
 
+    # def test_help(self):
+    #     """ Running the program without arguments should show usage. """
 
-    def test_help(self):
-        """ Running the program without arguments should show usage. """
+    #     # Run the command `python ./echo.py -h` in a separate process, then
+    #     # collect it's output.
+    #     process = subprocess.Popen(
+    #         ["python", "./echo.py", "-h"],
+    #         stdout=subprocess.PIPE)
+    #     stdout, _ = process.communicate()
+    #     with open("USAGE") as f:
+    #         usage = f.read()
 
-        # Run the command `python ./echo.py -h` in a separate process, then
-        # collect it's output.
-        process = subprocess.Popen(
-            ["python", "./echo.py", "-h"],
-            stdout=subprocess.PIPE)
-        stdout, _ = process.communicate()
-        with open("USAGE") as f:
-            usage = f.read()
-
-        self.assertEquals(stdout, usage)
+    #     self.assertEquals(stdout, usage)
 
 
 if __name__ == '__main__':
