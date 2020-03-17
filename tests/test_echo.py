7aeb56a6196d680921bc090645b9b749336816bc
Christian Rojas <cjrojas72@gmail.com>
2020-03-16 20:33:38 -0400

    working sol

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -9,30 +9,34 @@ import subprocess
 
 
 class TestEcho(unittest.TestCase):
-    def setUP(self):
+
+    def setUp(self):
         self.parser = echo.create_parser()
     
-    # def tests_no_options(self):
-    #     #self.fail("Need to write test")
-    #     args = ["HelLo WoRld"]
-    #     ns = self.parser.parse_args(args)
-    #     self.assertFalse(ns.upper)
-    #     self.assertFalse(ns.lower)
-    #     self.assertFalse(ns.title)
-    #     actual = echo.main(args)
-    #     expected = args[0]
-    #     self.assertEqual(actual, expected)
+    def tests_no_options(self):
+        #self.fail("Need to write test")
+        args = ["HelLo WoRld"]
+        ns = self.parser.parse_args(args)
+        self.assertFalse(ns.upper)
+        self.assertFalse(ns.lower)
+        self.assertFalse(ns.title)
+        actual = echo.main(args)
+        expected = args[0]
+        self.assertEqual(actual, expected)
     
-    # def tests_all_options(self):
-    #     #self.fail("Need to write test")
-    #     args = ["-tul", "HelLo WoRld"]
-    #     ns = self.parser.parse_args(args)
-    #     self.assertTrue(ns.upper)
-    #     self.assertTrue(ns.lower)
-    #     self.assertTrue(ns.title)
-    #     actual = echo.main(args)
-    #     expected = "Hello World"
-    #     self.assertEqual(actual, expected)
+    def tests_all_options(self):
+        #self.fail("Need to write test")
+        args = ["-tul", "HelLo WoRld"]
+        try:
+            ns = self.parser.parse_args(args)
+        except Exception as e:
+            print(str(e))
+        self.assertTrue(ns.upper)   
+        self.assertTrue(ns.lower)
+        self.assertTrue(ns.title)
+        actual = echo.main(args)
+        expected = "Hello World"
+        self.assertEqual(actual, expected)
 
     def tests_upper_short(self):
         #self.fail("Need to write test")
@@ -89,19 +93,19 @@ class TestEcho(unittest.TestCase):
         self.assertEqual(actual, expected)
 
 
-    # def test_help(self):
-    #     """ Running the program without arguments should show usage. """
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
 
-    #     # Run the command `python ./echo.py -h` in a separate process, then
-    #     # collect it's output.
-    #     process = subprocess.Popen(
-    #         ["python", "./echo.py", "-h"],
-    #         stdout=subprocess.PIPE)
-    #     stdout, _ = process.communicate()
-    #     with open("USAGE") as f:
-    #         usage = f.read()
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        with open("USAGE") as f:
+            usage = f.read()
 
-    #     self.assertEquals(stdout, usage)
+        self.assertEquals(stdout, usage)
 
 
 if __name__ == '__main__':
